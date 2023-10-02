2ea6c36cef5eb0e2701077df22c2c150855e16a0
abhinash_giri <56127409+abhinashgiri@users.noreply.github.com>
2023-10-02 11:41:26 +0530

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,52 +1,38 @@
-
 #include "bits/stdc++.h"
 using namespace std;
+
 template <typename T>
-class BIT
-{
+class BIT{
     int n;
     vector<T> bit;
 public :
-    BIT(int tree_size)
-    {
+    BIT(int tree_size){
         n = tree_size;
         bit.resize(n + 5) ;
     }
 
-    bool update(int x, T val)
-    {
+    bool update(int x, T val){
         if(x>n or x<1)return false;
-        for (; x <= n; x += x & -x)
-        {
+        for (; x <= n; x += x & -x){
             bit[x] += val;
         }
         return true;
     }
-    T at_position(int x)
-    {
-        if(x<1 or x>n) return -1;
-        return bit[x];
-    }
 
-    T query(int x)
-    {
+    T query(int x){
         T sum = 0;
         for (; x > 0; x -= x & -x)
             sum += bit[x];
         return sum;
     }
 
-    T query(int l, int r)
-    {
+    T query(int l, int r){
         return query(r) - query(l - 1) ;
     }
-    T lower_bound(T sum)
-    {
+    T lower_bound(T sum){
         T curr=0,prev=0;
-        for (int i = log2(n); i>=0 ; --i)
-        {
-            if( ((curr+( 1LL<<i))<=n) and (bit[curr+(1LL<<i)]+prev)<sum)
-            {
+        for (int i = log2(n); i>=0 ; --i){
+            if( ((curr+( 1LL<<i))<=n) && (bit[curr+(1LL<<i)]+prev)<sum){
                 curr+=(1LL<<i);
                 prev+=bit[curr];
             }
@@ -54,16 +40,13 @@ public :
         return curr+1;
     }
 };
-int main()
-{
+
+
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     BIT<int>bt(10);
-
-
-
-
     
     return 0;
 }
