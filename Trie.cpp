ab417ae86bb9dc174f9764d56b3cdfabecc93d9f
abhinash_giri <56127409+abhinashgiri@users.noreply.github.com>
2023-10-02 18:56:33 +0530

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -6,18 +6,15 @@ class Trie{
 public:
     int cnt;
     Trie* child[2];
-    Trie()
-    {
+    Trie(){
         this->cnt=0;
         this->child[0] = nullptr;
         this->child[1] = nullptr;
     }
 
-    void insert(ll x,Trie* &p)
-    {
+    void insert(ll x,Trie* &p){
         Trie *root = p;
-        for (int i = mx_bit; i >=0 ; --i)
-        {
+        for (int i = mx_bit; i >=0 ; --i){
             int d = (x>>i)&1;
             if(root->child[d]== nullptr)root->child[d] = new Trie();
             root=root->child[d];
@@ -25,17 +22,14 @@ public:
         }
     }
 
-    int remove(ll x,Trie* root,int i=mx_bit)
-    {
+    int remove(ll x,Trie* root,int i=mx_bit){
         if(i<0)return 1;
         if(root== nullptr)return 0;
         int d=(x>>i)&1;
-        if(root->child[d])
-        {
+        if(root->child[d]){
             int g=remove(x,root->child[d],i-1);
             root->child[d]->cnt-=g;
-            if(root->child[d]->cnt==0)
-            {
+            if(root->child[d]->cnt==0){
                 delete root->child[d];
                 root->child[d] = nullptr;
                 return 1;
@@ -45,25 +39,19 @@ public:
         return 0;
     }
 
-    int maxXOR(int a,Trie* &p)
-    {
+    int maxXOR(int a,Trie* &p){
         Trie*root =p;
         int ans=0;
-        for (int i = mx_bit; i >=0 ; --i)
-        {
-            if((1<<i)&a)
-            {
-                if(root->child[0]!= nullptr)
-                {
+        for (int i = mx_bit; i >=0 ; --i){
+            if((1<<i)&a){
+                if(root->child[0]!= nullptr){
                     ans|=(1<<i);
                     root=root->child[0];
                 }
                 else root=root->child[1];
             }
-            else
-            {
-                if(root->child[1]!= nullptr)
-                {
+            else{
+                if(root->child[1]!= nullptr){
                     ans|=(1<<i);
                     root=root->child[1];
                 }
@@ -73,33 +61,25 @@ public:
         return ans;
     }
 
-    int minXOR(int a,Trie* &p)
-    {
+    int minXOR(int a,Trie* &p){
         Trie*root =p;
         int ans=0;
-        for (int i = mx_bit; i >=0 ; --i)
-        {
-            if((1<<i)&a)
-            {
-                if(root->child[1]!= nullptr)
-                {
+        for (int i = mx_bit; i >=0 ; --i){
+            if((1<<i)&a){
+                if(root->child[1]!= nullptr){
 
                     root=root->child[1];
                 }
-                else
-                {
+                else{
                     ans|=(1<<i);
                     root=root->child[0];
                 }
             }
-            else
-            {
-                if(root->child[0]!= nullptr)
-                {
+            else{
+                if(root->child[0]!= nullptr){
                     root=root->child[0];
                 }
-                else
-                {
+                else{
                     ans|=(1<<i);
                     root=root->child[1];
                 }
@@ -109,31 +89,25 @@ public:
     }
 
     // count of entries a s.t  ( a xor x ) >=k
-    long long int greaterThan(Trie* root,int x,int k)
-    {
+    long long int greaterThan(Trie* root,int x,int k){
         long long int ans=0;
-        for (int i = mx_bit; i >=0 ; --i)
-        {
+        for (int i = mx_bit; i >=0 ; --i){
             int bit = ((x&(1<<i))!=0);
             // ith bit is set on both x and k
-            if((x&(1<<i)) and (k&(1<<i)))
-            {
+            if((x&(1<<i)) and (k&(1<<i))){
                 if(root->child[0]== nullptr)return ans;
                 root=root->child[0];
             }
-            else if((x&(1<<i)) and !(k&(1<<i)))
-            {
+            else if((x&(1<<i)) and !(k&(1<<i))){
                 if(root->child[0]!= nullptr) ans+=root->child[0]->cnt;
                 if(root->child[1]== nullptr)return ans;
                 root=root->child[1];
             }
-            else if(!(x&(1<<i)) and (k&(1<<i)))
-            {
+            else if(!(x&(1<<i)) and (k&(1<<i))){
                 if(root->child[1]== nullptr)return ans;
                 root=root->child[1];
             }
-            else if(!(x&(1<<i)) and !(k&(1<<i)))
-            {
+            else if(!(x&(1<<i)) and !(k&(1<<i))){
                 if(root->child[1]!= nullptr) ans+=root->child[1]->cnt;
                 if(root->child[0]== nullptr)return ans;
                 root=root->child[0];
@@ -143,41 +117,35 @@ public:
         return ans;
     }
 
-    void printAll(Trie*p,long long int ans=0,int i=mx_bit)
-    {
-        if(p == nullptr)
-        {
+    void printAll(Trie*p,long long int ans=0,int i=mx_bit){
+        if(p == nullptr){
             return;
         }
-        if(i<0)
-        {
+        if(i<0){
             cerr<<ans<<" ";
             return;
         }
-        if(p->child[0]!= nullptr)
-        {
+        if(p->child[0]!= nullptr){
             printAll(p->child[0],ans,i-1);
         }
-        if(p->child[1]!= nullptr)
-        {
+        if(p->child[1]!= nullptr){
             printAll(p->child[1],ans|(1LL<<i),i-1);
         }
     }
 };
 
-int main()
-{
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
     return 0;
 }
